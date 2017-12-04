827e8d5aa27e89d7ecafa431f4b3f2bb2cd5064e
liujia <liujia1234505@sina.com>
2017-12-04 12:03:44 +0800

    ceshi

--- test.php
+++ test.php
@@ -2,5 +2,5 @@
 require_once __DIR__ . '/vendor/autoload.php';  
 use HolidayOrWorkday\holidayOrWorkday;  
 
-
+//ceshi
 var_dump( (new holidayOrWorkday('d6fe2c62790e47db6f904e409483dc72'))->isWorkday('2017-12-01'));
